55e330a9d545a69244e1df074def665a9f901308
AbiollaghJames <abiollajames@protonmail.com>
2023-10-05 13:30:17 +0300

    Complex types - string and int/float to tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+""" Using union to combine int and float """
 
 from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ return str and Union of int or float """
     return (k, (v**2))
