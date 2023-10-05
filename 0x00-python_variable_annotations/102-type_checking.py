2cbcd64b3b3cd8ab0c16056e1dcdd2998366077f
AbiollaghJames <abiollajames@protonmail.com>
2023-10-05 14:52:21 +0300

    Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python3
+""" type checking using myPy """
+
 from typing import Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    """ returns a tuple """
     zoomed_in: Tuple = tuple(
         item for item in lst
         for i in range(factor)
     )
 
     return zoomed_in
+
 
 array = (12, 72, 91)
 zoom_2x = zoom_array(array)
